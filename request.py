0fa87e2341077d507ec429ddea345a734fdf0642
joshua1955 <mrantidod@gmail.com>
2020-08-18 20:41:49 +0700

    Edited example

--- request.py
+++ request.py
@@ -7,5 +7,27 @@ lic_chet = '70033'
 
 cookies=eltex_lte_lib.LteLoginCookies(login,password,ip)
 dict=eltex_lte_lib.Data(lic_chet,cookies)
+print("Main inf about ont terminal:")
+print('\nUNI0')
+print(dict[0])
+print('\nUNI1')
+print(dict[1])
+print('\nMain data')
+print(dict[2])
+
+print('\nUNI0 received stats')
+print(eltex_lte_lib.DataPonStats(dict[2].get('MAC'),'UNI0',cookies)[0])
+
+print('\nUNI0 transmited stats')
 print(eltex_lte_lib.DataPonStats(dict[2].get('MAC'),'UNI0',cookies)[1])
+
+print('\nPON received stats')
+print(eltex_lte_lib.DataPonStats(dict[2].get('MAC'),'PON',cookies)[0])
+
+print('\nPON transmited stats')
+print(eltex_lte_lib.DataPonStats(dict[2].get('MAC'),'PON',cookies)[1])
+
+print('\nLTE logs')
+print(eltex_lte_lib.LteLogs(cookies))
+
 eltex_lte_lib.LteLogout(cookies)
